48ee11c76bf2c2bea0c8e5c5831ab75fd3e97a06
LaoZ <zhaoxiantao@pset.suntec.net>
2015-06-16 15:01:45 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,3 @@
-/*!
- * Copyright @ 2011 - 2014 Suntec Software(Shanghai) Co., Ltd.
- * All Rights Reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are NOT permitted except as agreed by
- *  Suntec Software(Shanghai) Co., Ltd.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- */
 'use strict';
 
 module.exports = function(grunt) {
